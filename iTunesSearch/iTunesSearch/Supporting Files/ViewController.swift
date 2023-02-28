d78971b98fecf37ca82d2f0c9503132a9bf56c78
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-02-28 09:16:16 -0600

    Commits initial project

---
+++ iTunesSearch/iTunesSearch/Supporting Files/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesSearch
+//
+//  Created by iMac Pro on 2/28/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
