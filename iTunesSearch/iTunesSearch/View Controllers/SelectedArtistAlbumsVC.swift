f57c7870ae30daa06383f7b3de1136ac5cf81241
pedrocerrano <82617951+pedrocerrano@users.noreply.github.com>
2023-03-02 07:54:05 -0600

    Updates Initial layout design.

---
+++ iTunesSearch/iTunesSearch/View Controllers/SelectedArtistAlbumsVC.swift
@@ -0,0 +1,28 @@
+//
+//  SelectedArtistAlbumsVC.swift
+//  iTunesSearch
+//
+//  Created by iMac Pro on 3/2/23.
+//
+
+import UIKit
+
+class SelectedArtistAlbumsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
